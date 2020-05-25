7454c5db9d0e6f5dda755c3e3cfbf8e1c7f9da5b
unknown <XEON>
2020-05-25 15:54:45 +0800

    添加 DHT11 温湿度模块

--- weather/sensor/DHT11/test.py
+++ weather/sensor/DHT11/test.py
@@ -6,16 +6,19 @@ import ujson
 
 sensor = dht.DHT11(Pin(14))
 
-while True:
-  try:
-    sensor.measure()
-    temperature = sensor.temperature()
-    humidity = sensor.humidity()
-    data = dict(t=temperature, h=humidity)
-    payload = ujson.dumps(data)
-    url = 'http://192.168.2.2:1111/'
-    response = urequests.post(url, data=payload)
-    print('Temperature: {:3.1f} C, Humidity: {:3.1f} %'.format(temperature, humidity))
-    time.sleep(59)
-  except OSError as e:
-    print('Failed to read sensor.')
+def DHTWorker():
+    while True:
+      try:
+        sensor.measure()
+        temperature = sensor.temperature()
+        humidity = sensor.humidity()
+        data = dict(t=temperature, h=humidity)
+        payload = ujson.dumps(data)
+        url = 'http://192.168.2.2:1111/'
+        response = urequests.post(url, data=payload)
+        print('Temperature: {:3.1f} C, Humidity: {:3.1f} %'.format(temperature, humidity))
+        time.sleep(5)
+      except OSError as e:
+        print('Failed to read sensor.')
+
+DHTWorker()
