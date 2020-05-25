5397530f5eda3c2fe3014a631b779c51d7b65911
unknown <XEON>
2020-05-25 15:55:28 +0800

    添加 webrepl 配置文件

--- weather/main.py
+++ weather/main.py
@@ -1,5 +1,4 @@
 import network, webrepl
-# from sensor import jobs
 
 wlan = network.WLAN(network.STA_IF)
 if not wlan.isconnected():
@@ -10,4 +9,3 @@ if not wlan.isconnected():
         pass
 print('Network config:', wlan.ifconfig())
 webrepl.start()
-# jobs(60, 5)
